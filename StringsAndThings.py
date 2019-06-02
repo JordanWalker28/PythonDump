495be67488031b98a2219062b980c3743c65c3f0
Jordan Walker <jordanswalker28@gmail.com>
2019-06-02 16:42:27 +0100

    new updates

--- StringsAndThings.py
+++ StringsAndThings.py
@@ -1,1 +1,43 @@
+# <--- Single Line comment
+
+"""
+    This is a multi line comment
+"""
+
+#Generic Print Statement
 print("Hello")
+print("My name is")
+
+#new line
+print("\n")
+
+#system sound
+print("\a")
+
+#print a statement multiple times
+print("Hello " * 10)
+
+#print calculations
+print(1+2+3)
+
+#Can use single or double quotes
+print("Hello Jordan")
+print('Hello')
+
+#Can use quotes between character i.e
+#If you start with one you muse end with one though
+print("Jordan's tall")
+print('Jordan has been known to "program" late into the night')
+
+#concat
+print("Jordans" + " quick" + " guide")
+
+#print variables
+greeting = "Hello "
+name = "Jordan"
+print(greeting + name)
+
+# " " <--- space
+# "" <--- empty 
+
+
